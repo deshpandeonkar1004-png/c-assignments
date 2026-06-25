4ac35787e4d6096f0b3bd829f5901730279216b4
Onkar Deshpande <deshpandeonkar1004@gmail.com>
2026-06-25 17:14:13 +0530

    Added Assignmnet

--- assignment-9.c
+++ assignment-9.c
@@ -16,7 +16,62 @@ int main (){
     }
     return 0;
 }
-*/
+//Q2
 int main(){
-    int 
+    int a,b,c,d,e;
+    float result;
+    printf("Enter 5 subject marks of 100:");
+    scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+    result=(a+b+c+d+e)/5.0;
+    if(result>=33){
+        printf("Pass %.2f",result);
+    }
+    else{
+        printf("Fail %.2f",result);
+    }
+    return 0;
 }
+
+//Q3
+int main()
+{
+    char ch;
+    printf("Enter a character:");
+    scanf("%c",&ch);
+    if(ch >='A' && ch<='Z'){
+        printf("Upper case %c",ch);
+    }
+    else if(ch >='a' && ch <='z'){
+        printf("Lower case %c",ch);
+    }
+    return 0;
+}
+
+//Q4
+int main(){
+    int a;
+    printf("Enter a Number:");
+    scanf("%d",&a);
+    if(a%3==0 && a%2==0){
+        printf("Divisible by 3 and 2");
+    }
+    else{
+        printf("Not divisible by 3 and 2");
+    }
+    return 0;
+
+//Q5
+int main(){
+    int a;
+    printf("Enter a number:");
+    scanf("%d",&a);
+    if(a%7==0 || a%3==0){
+        printf("Number is divisible by 7 or 3");
+    }
+    else{
+        printf("Number is not divisible by 7 or 3");
+    }
+    return 0;
+}
+    */
+   
---
+++ assignment10.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+//Q1
+/*
+int main (){
+    int a;
+    printf("Enter a number:");
+    scanf("%d",&a);
+    if(a==0){
+        printf("Zero");
+    }
+    else if(a>0){
+        printf("Positive");
+    }
+    else{
+        printf("Negative");
+    }
+    return 0;
+}
+
+//Q2
+int main(){
+    char ch;
+    printf("Enter Something:");
+    scanf("%c",&ch);
+    if(ch >='0' & ch <='9'){
+        printf("Digit");
+    }
+    else if(ch >='A' && ch <='Z'){
+        printf("Upper case");
+    }
+    else if(ch >='a' && ch <='z'){
+        printf("Lower case");
+    }
+    else{
+        printf("Special character");
+    }
+    return 0;
+}
+
+//Q3
+int main(){
+    int a,b,c;
+    printf("Enter sides of triangle:");
+    scanf("%d %d %d",&a,&b,&c);
+    if((a+b>c) && (b+c>a) && (c+a>b)){
+        printf("Valid triangle");
+    }
+    else{
+        printf("Invalid Triangle");
+    }
+    return 0;
+}
+
+//Q4
+int main(){
+    int a;
+    printf("Enter month number:");
+    scanf("%d",&a);
+    if(a==4 || a==6 || a==9 || a==11){
+        printf("30 Days");
+    }
+    else if(a==2){
+        printf("28 or 29 Days");
+    }
+    else{
+        printf("31 Days");
+    }
+}
+*/
