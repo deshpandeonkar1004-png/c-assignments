7d8aecf4e58b91e45895a01d55e837ba245208be
Onkar Deshpande <deshpandeonkar1004@gmail.com>
2026-06-26 23:54:47 +0530

    New Assignment

---
+++ assignmnet13.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+//Q1
+/*
+int main(){
+    int i,a,sum=0;
+    printf("Enter a number:");
+    scanf("%d",&a);
+    for(i=1;i<=a;i++){
+        sum=sum+i;
+    }
+    printf("%d",sum);
+    return 0;
+}
+
+//Q2
+int main(){
+    int i=2,n,sum=0;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    while(i<=2*n){
+        sum+=i;
+        i=i+2;
+    }
+    printf("%d",sum);
+}
+
+//Q3
+int main(){
+    int i=1,n,sum=0;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    while(i<=2*n){
+        sum+=i;
+        i=i+2;
+    }
+    printf("%d",sum);
+}
+    
+//Q4
+int main(){
+    int i=1,n,sum=0;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    while(i<=n){
+        sum+=i*i;
+        i++;
+    }
+    printf("%d",sum);
+
+}
+
+//Q5
+int main(){
+    int i=1,n,sum=0;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    while(i<=n){
+        sum+=i*i*i;
+        i++;
+    }
+    printf("%d",sum);
+
+}
+*/
